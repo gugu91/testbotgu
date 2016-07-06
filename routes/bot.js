31f7a2f54558168d0fd32e9c392dd21a4a803d45
Gugu <gporce@gmail.com>
2016-07-06 19:07:21 +0200

    Ok

--- routes/bot.js
+++ routes/bot.js
@@ -2,58 +2,49 @@
 const Bot = require('messenger-bot');
 const GuMessageEngine = require('../lib/engines/guMessageEngine.js');
 const GuPostbackEngine = require('../lib/engines/guPostbackEngine.js');
-const GuSession = require('../lib/session/guSession.js');
 const express = require('express');
 
 let router = express.Router();
-let messsageEngine = new GuMessageEngine();
+let messageEngine = new GuMessageEngine();
 let postbackEngine = new GuPostbackEngine();
-let sessionManager = new GuSession();
 let bot = new Bot({
     token: 'EAADtS47PWpoBAC068H525YNbvst47A8ewmeaB4kOrl1orZB3UfJQoa8aaZBZC5zSZB6T8YXjK87uYxVvsgJaGHNSV8WFVqmcDzgl4pCTUZB8LjD9zY9sUA8tpQQtNu8w94JNnawJBhtiZBqG1EYk43se6zQ98NrwrQlzKFvjj5EwZDZD',
     verify: 'VERIFY_TOKEN',
     app_secret: '536ee5ee81645557826fd89543c56886'
 });
 
-function getSessionAsync(fbId, callback) {
-    let session = sessionManager.getSession(fbId);
-    if (!session.getContext().profile) {
-        bot.getProfile(payload.sender.id, (err, profile) => {
-            if (err) throw err
-
-            reply(responseEngine.generic(), (err) => {
-                if (err) throw err
-
-                session.addToContext(profile);
-                console.log(`Retrieved profile info for ${fbId}: ${profile.first_name} ${profile.last_name}`);
-                if (callback)
-                    callback();
-            });
-        });
-    } else if (callback) {
-        callback();
-    }
-};
-
-
 bot.on('error', (err) => {
     console.log(err.message)
 })
 
 bot.on('message', (payload, reply) => {
-    console.log(`Received message from ${payload.sender.id}`);    
-    getSessionAsync(payload.sender.id, () => {
-        reply(messageEngine.process(payload, context), (err) => {
+    console.log(`Received message from ${payload.sender.id}`);
+
+    messageEngine.process(payload, (response) => {
+        reply(response, (err) => {
             if (err) throw err
+
         });
     });
+
+    //   bot.getProfile(payload.sender.id, (err, profile) => {
+    //     if (err) throw err
+
+    //     reply(responseEngine.generic(), (err) => {
+    //       if (err) throw err
+
+    //       console.log(`Received message from ${profile.first_name} ${profile.last_name}:`)
+    //     });
+    //   });
 });
 
 bot.on('postback', (payload, reply) => {
     console.log(`Received postback from ${payload.sender.id}`);
-    getSessionAsync(payload.sender.id, () => {
-        reply(postbackEngine.process(payload, context), (err) => {
+
+    postbackEngine.process(payload, (response) => {
+        reply(response, (err) => {
             if (err) throw err
+
         });
     });
 });
