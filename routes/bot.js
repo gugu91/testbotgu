f361704670a52439ee28da339ba427ff63064153
Gugu <gporce@gmail.com>
2016-07-06 17:24:58 +0200

    added session

--- routes/bot.js
+++ routes/bot.js
@@ -1,54 +1,70 @@
 'use strict'
 const Bot = require('messenger-bot');
-let express = require('express');
-const GuResponse = require('./guResponse.js');
+const GuMessageEngine = require('../lib/engines/guMessageEngine.js');
+const GuPostbackEngine = require('../lib/engines/guPostbackEngine.js');
+const GuSession = require('../lib/session/guSession.js');
+const express = require('express');
+
 let router = express.Router();
-
-let responseEngine = new GuResponse();
-
+let messsageEngine = new GuMessageEngine();
+let postbackEngine = new GuPostbackEngine();
+let sessionManager = new GuSession();
 let bot = new Bot({
-  token: 'EAADtS47PWpoBAC068H525YNbvst47A8ewmeaB4kOrl1orZB3UfJQoa8aaZBZC5zSZB6T8YXjK87uYxVvsgJaGHNSV8WFVqmcDzgl4pCTUZB8LjD9zY9sUA8tpQQtNu8w94JNnawJBhtiZBqG1EYk43se6zQ98NrwrQlzKFvjj5EwZDZD',
-  verify: 'VERIFY_TOKEN',
-  app_secret: '536ee5ee81645557826fd89543c56886'
+    token: 'EAADtS47PWpoBAC068H525YNbvst47A8ewmeaB4kOrl1orZB3UfJQoa8aaZBZC5zSZB6T8YXjK87uYxVvsgJaGHNSV8WFVqmcDzgl4pCTUZB8LjD9zY9sUA8tpQQtNu8w94JNnawJBhtiZBqG1EYk43se6zQ98NrwrQlzKFvjj5EwZDZD',
+    verify: 'VERIFY_TOKEN',
+    app_secret: '536ee5ee81645557826fd89543c56886'
 });
 
-// bot.on('error', (err) => {
-//   console.log(err.message)
-// })
+function getSessionAsync(fbId, callback) {
+    let session = sessionManager.getSession(fbId);
+    if (!session.getContext().profile) {
+        bot.getProfile(payload.sender.id, (err, profile) => {
+            if (err) throw err
+
+            reply(responseEngine.generic(), (err) => {
+                if (err) throw err
+
+                session.addToContext(profile);
+                console.log(`Retrieved profile info for ${fbId}: ${profile.first_name} ${profile.last_name}`);
+                if (callback)
+                    callback();
+            });
+        });
+    } else if (callback) {
+        callback();
+    }
+};
+
+
+bot.on('error', (err) => {
+    console.log(err.message)
+})
 
 bot.on('message', (payload, reply) => {
-  let text = payload.message.text
-    reply(responseEngine.generic(), (err) => {
-      if (err) throw err
-
-      console.log(`Received message from ${payload.sender.id}`)
+    console.log(`Received message from ${payload.sender.id}`);    
+    getSessionAsync(payload.sender.id, () => {
+        reply(messageEngine.process(payload, context), (err) => {
+            if (err) throw err
+        });
     });
-//   bot.getProfile(payload.sender.id, (err, profile) => {
-//     if (err) throw err
-
-//     reply(responseEngine.generic(), (err) => {
-//       if (err) throw err
-
-//       console.log(`Received message from ${profile.first_name} ${profile.last_name}:`)
-//     });
-//   });
 });
 
 bot.on('postback', (payload, reply) => {
-    reply(responseEngine.postBack(), (err) => {
-      if (err) throw err
-
-      console.log(`Received postback from ${payload.sender.id}`)
+    console.log(`Received postback from ${payload.sender.id}`);
+    getSessionAsync(payload.sender.id, () => {
+        reply(postbackEngine.process(payload, context), (err) => {
+            if (err) throw err
+        });
     });
 });
 
 router.get('/', (req, res) => {
-  return bot._verify(req, res)
+    return bot._verify(req, res)
 });
 
 router.post('/', (req, res) => {
-  bot._handleMessage(req.body)
-  res.end(JSON.stringify({status: 'ok'}))
+    bot._handleMessage(req.body)
+    res.end(JSON.stringify({ status: 'ok' }))
 });
 
 module.exports = router;
