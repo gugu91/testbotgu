eecb3ab3a582b2ec6d33d67c2169da091a7cc2e9
Gugu <gporce@gmail.com>
2016-07-14 20:10:50 +0200

    Added chat template

--- public/js/angular/app.js
+++ public/js/angular/app.js
@@ -1,6 +1,6 @@
-angular.module("guBotApp", ["ngRoute", "ngResource", "ngSanitize","ngAnimate"])
+angular.module("guBotApp", ["ngRoute", "ngResource", "ngSanitize", "ngAnimate"])
     .controller("homeController",
-        function() {
-            var self = this;
-            self.title = "Gu Bot";
-        });
+    function () {
+        var self = this;
+        self.title = "Gu Bot";
+    });
