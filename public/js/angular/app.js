70357a230e0d08532c678683fe0b10a707f93d77
Gugu <gporce@gmail.com>
2016-07-15 12:28:59 +0200

    Added logo

--- public/js/angular/app.js
+++ public/js/angular/app.js
@@ -3,4 +3,13 @@ angular.module("guBotApp", ["ngRoute", "ngResource", "ngSanitize", "ngAnimate"])
     function () {
         var self = this;
         self.title = "Gu Bot";
+    })
+    .run(
+    function ($rootScope, $location, $timeout) {
+        //this is important beacause web page is designed dynamically ing ng and mdl needs it
+        $rootScope.$on('$viewContentLoaded', function () {
+            $timeout(function () {
+                componentHandler.upgradeAllRegistered();
+            });
+        });
     });
